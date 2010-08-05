628b2228187c2191bf6ee485c2fa0d7aa7361eeb
Brenton Ashworth <bashwort@gmail.com>
2010-08-05 10:48:25 -0700

    Change version to 0.1.1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-growl "0.1.0"
+(defproject clj-growl "0.1.1"
   :description "Send Growl notifications from Clojure."
   :dependencies [[org.clojure/clojure "1.2.0-RC1"]
                  [org.clojure/clojure-contrib "1.2.0-RC1"]]
