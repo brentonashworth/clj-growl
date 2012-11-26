e151b3bd73327d80c5aa9332f8e7172728888419
Ponkore <ponkore@me.com>
2012-11-26 20:51:00 +0900

    Clojure 1.2.0-RC1 does not exist, already disappeared from maven repositories. So, I modified clojure version (from 1.2.0-RC1 to 1.4.0, and removed clojure-contrib).

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
 (defproject clj-growl "0.2.1"
   :description "Send Growl notifications from Clojure."
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[jline "0.9.94"]])
